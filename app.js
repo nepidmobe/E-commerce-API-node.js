2dfa99bf9f5101b8ca86e4eea4dc8cda49ec2dc3
Dipen Thapa <nepidtube@gmail.com>
2022-04-24 12:12:57 +0545

    mongoose 6 no deprication warning

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connectDB = require("./db/connect");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port, () => {
+      console.log("listening on port...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
